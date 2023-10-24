339c38a577780cf757f25dea818626ab0f3f33e8
chenlongzu <1045683477@qq.com>
2023-10-24 16:18:04 +0800

    feat:

--- lua/plugins/extras/code/flutter-tools.lua
+++ lua/plugins/extras/code/flutter-tools.lua
@@ -1,10 +1,30 @@
 -- flutter lsp
 return {
-    'akinsho/flutter-tools.nvim',
-    lazy = false,
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        'stevearc/dressing.nvim', -- optional for vim.ui.select
+    {
+        'akinsho/flutter-tools.nvim',
+        lazy = false,
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'stevearc/dressing.nvim', -- optional for vim.ui.select
+        },
+        config = true,
     },
-    config = true,
+    -- {
+    --     'RobertPietraru/bloc.nvim',
+    --     keys = { {
+    --         "<leader>cbc",
+    --         "<cmd>require('bloc').create_cubit('cubit_name', 'parent/folder/relative/path')<cr>",
+    --         -- function ()
+    --         --     require('bloc').create_cubit('cubit_name', 'parent/folder/relative/path')
+    --         -- end,
+    --         desc = "bloc cubit"
+    --     }, {
+    --         "<leader>cbb",
+    --         "<cmd>require('bloc').create_bloc('bloc_name', 'parent/folder/relative/path')<cr>",
+    --         -- function ()
+    --         --     require('bloc').create_bloc('bloc_name', 'parent/folder/relative/path')
+    --         -- end,
+    --         desc = "bloc bloc"
+    --     } },
+    -- }
 }
