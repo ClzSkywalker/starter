2dd18789eb202d1d1b486cbd072c6bc9b90ca984
longzu.chen <longzu.chen@kunlun-inc.com>
2025-02-17 16:22:05 +0800

    feat: update mason

--- lua/plugins/lazy_config.lua
+++ lua/plugins/lazy_config.lua
@@ -7,18 +7,42 @@ return {
     { import = "lazyvim.plugins.extras.ai.copilot" },
     -- Better Yank/Paste
     { import = "lazyvim.plugins.extras.coding.yanky" },
-    -- (),[],<> 
-    { import = "lazyvim.plugins.extras.coding.mini-surround"},
+    -- (),[],<>
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
     -- Increment and decrement numbers, dates, and more
-    { import = "lazyvim.plugins.extras.editor.dial"},
+    { import = "lazyvim.plugins.extras.editor.dial" },
     -- Incremental LSP renaming based on Neovim's command-preview feature
-    { import = "lazyvim.plugins.extras.editor.inc-rename"},
+    { import = "lazyvim.plugins.extras.editor.inc-rename" },
     -- Fast and modern file picker
-    { import = "lazyvim.plugins.extras.editor.snacks_picker"},
+    { import = "lazyvim.plugins.extras.editor.snacks_picker" },
     -- Neotest support. Requires language specific adapters to be configured. (see lang extras)
-    { import = "lazyvim.plugins.extras.test.core"},
+    { import = "lazyvim.plugins.extras.test.core" },
     -- Language support for dotfiles
-    { import = "lazyvim.plugins.extras.util.dot"},
+    { import = "lazyvim.plugins.extras.util.dot" },
     -- Highlight colors in your code. Also includes Tailwind CSS support.
-    { import = "lazyvim.plugins.extras.util.mini-hipatterns"},
+    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    {
+        "stevearc/conform.nvim",
+        optional = true,
+        opts = {
+            formatters_by_ft = {
+                go = { "goimports-reviser" },
+            },
+        },
+    },
+    {
+        "williamboman/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "taplo", -- toml
+                "codelldb",
+                "goimports-reviser",
+                "gopls",
+                "prettier",
+                "stylua",
+                "shfmt"
+            },
+        },
+    },
 }
